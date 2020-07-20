abaf05ec6e1509c4c31def19c239040ee7061dc1
Troy Feng <troy.feng@yale.edu>
2020-07-20 16:09:07 -0400

    Implemented simple addition and subtraction with small bugs

--- calculator.js
+++ calculator.js
@@ -1,38 +1,125 @@
 var resDisplay = document.getElementById("res");
+var expDisplay = document.getElementById("exp");
 var numButtons = document.getElementsByClassName("numButtons");
+var ac = false;
+var justDisplayedAnswer = false;
+var justDisplayedEqual = false;
+var ansSoFar = 0;
+var thisChunk = 1;
+var curOp = '#';
+
 for (var i = 0, len = numButtons.length; i < len; i++) {
 	let x = numButtons[i].innerHTML;
     numButtons[i].onclick = function() {
-    	displayInResult(x);
+    	if (justDisplayedAnswer) {
+    		clearResDisplay();
+    		justDisplayedAnswer = false;
+    	}
+    	if (justDisplayedEqual) {
+    		clearExpDisplay();
+    		ansSoFar = 0;
+    		thisChunk = 1;
+			justDisplayedEqual = false;    		
+    	}
+    	if (ac) {
+    		ac = false;
+    		clearButton.innerHTML = 'C';
+    	}
+    	appendToExpDisplay(x);
+    	appendToResDisplay(x);
     }
 }
 
-var opButtons = document.getElementsByClassName("opButtons");
-for (var i = 0, len = opButtons.length; i < len; i++) {
-	let x = opButtons[i].innerHTML;
-	opButtons[i].onclick = function() {
-		displayInResult(x);
+var plusButton = document.getElementById("btnSum");
+plusButton.onclick = function() {
+	appendToExpDisplay("+");
+	opClicked();
+	curOp = '+';
+	thisChunk = 1;
+}
+
+var minusButton = document.getElementById("btnSub");
+minusButton.onclick = function() {
+	if (justDisplayedEqual) {
+		expDisplay.innerHTML = resDisplay.innerHTML;
+		justDisplayedEqual = false;
 	}
+	appendToExpDisplay("-");
+	opClicked();
+	curOp = '-';
+	thisChunk = 1;
 }
 
 var clearButton = document.getElementById("btnClr");
 clearButton.onclick = function() {
-	clearDisplay();
+	if (ac) {
+		clearExpDisplay();
+		ansSoFar = 0;
+		thisChunk = 1;
+	}
+	clearResDisplay();
+	ac = true;
+	clearButton.innerHTML = "AC";
 }
 
 var decimalButton = document.getElementById("btnDec");
 decimalButton.onclick = function() {
-	displayInResult(decimalButton.innerHTML);
+	appendToExpDisplay(decimalButton.innerHTML);
 }
 
-function displayInResult(x) {
+var equalsButton = document.getElementById("btnEql");
+equalsButton.onclick = function() {
+	opClicked();
+	appendToExpDisplay("=" + ansSoFar);
+	displayAnsSoFar();
+	justDisplayedEqual = true;
+	ac = true;
+	clearButton.innerHTML = "AC";
+}
+
+function displayAnsSoFar() {
+	resDisplay.innerHTML = ansSoFar;
+}
+
+function appendToExpDisplay(x) {
+	expDisplay.innerHTML += x;
+}
+
+function appendToResDisplay(x) {
 	resDisplay.innerHTML += x;
 }
 
-function clearDisplay() {
+function opClicked() {
+	var thisTerm = resDisplay.innerHTML;
+	var x = parseInt(thisTerm,10);
+	if (curOp=='#') {
+		ansSoFar = x;
+		thisChunk = x;
+	}
+	else if (curOp=='+') {
+		ansSoFar += thisChunk*x;
+		displayAnsSoFar();
+	}
+	else if (curOp=='-') {
+		ansSoFar -= thisChunk*x;
+		displayAnsSoFar();
+	}
+	justDisplayedAnswer = true;
+}
+
+function clearResDisplay() {
 	resDisplay.innerHTML = "";
+}
+
+function clearExpDisplay() {
+	expDisplay.innerHTML = "";
 }
 
 function displayError() {
 	resDisplay.innerHTML = "Invalid input";
+}
+
+function eval(exp) {
+	displayError();
+	justDisplayedAnswer = true;
 }
